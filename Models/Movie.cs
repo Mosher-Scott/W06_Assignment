885d588ba7d44661a378f3ff664e87d96f1976de
Scott Mosher <scott@scottmosherphotography.com>
2020-02-13 15:46:30 -0700

    More lessons

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -8,9 +9,13 @@ namespace RazorPagesMovie.Models
 
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")] // Sets the display name of the variable on the /movies index page
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")] // Allows the EF Core to correctly display the currency in the database
         public decimal Price { get; set; }
 
     }
