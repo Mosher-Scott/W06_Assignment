a52895929abd15db13bda8605b1787028f607235
Mosher-Scott <jorgemonkey81@byui.edu>
2020-02-15 14:59:59 -0700

    Finished Project

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,35 +7,49 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
 
+        //public int ID { get; set; }
+
+        //[StringLength(60, MinimumLength = 3)]
+        //[Required]
+        //public string Title { get; set; }
+
+        //[Display(Name = "Release Date")]  // Sets the display name of the variable on the /movies index page
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //public DateTime ReleaseDate { get; set; }
+
+        //[Range(1, 100)]
+        //[DataType(DataType.Currency)]
+        //[Column(TypeName = "decimal(18, 2)")]  // Allows the EF Core to correctly display the currency in the database
+        //public decimal Price { get; set; }
+
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        //[Required]
+        //[StringLength(30)]
+        //public string Genre { get; set; }
+
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        //[StringLength(5)]
+        //[Required]
+        //public string Rating {get; set;}
+
         public int ID { get; set; }
 
         [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]  // Sets the display name of the variable on the /movies index page
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
-        [Column(TypeName = "decimal(18, 2)")]  // Allows the EF Core to correctly display the currency in the database
-        public decimal Price { get; set; }
-
-        //[Display(Name = "Release Date")] // Sets the display name of the variable on the /movies index page
-        //[DataType(DataType.Date)]
-        //public DateTime ReleaseDate { get; set; }
-
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [Required]
-        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
-        public string Rating {get; set;}
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string Rating { get; set; }
 
     }
 }
